f6a3c5659bba8f5dd9eb7a92d242d21e980974f9
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2021-12-12 21:34:09 +0300

    Restructure project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="cros_solver",
+    version="0.1",
+    packages=["cros_solver"],
+    url="https://github.com/MIPT-Virtual-Labs/CrosSolver.git",
+    author="Denis Mitroshin",
+    author_email="mitroshin.da@phystech.edu",
+    include_package_data=True,
+    # package_data={"": ["./bin/*"]},
+)
